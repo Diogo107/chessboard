e69b88287bfad822e5b1295a03bbfe8ac6317943
Diogo Santos <diogo.filipe.santos107@gmail.com>
2020-04-09 20:09:09 +0100

    Updates

---
+++ precache-manifest.a0cb795d4b7bb71a4d022653a62d75d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3db843f1c70ec76074f73f1d65e912f1",
+    "url": "/chessboard/index.html"
+  },
+  {
+    "revision": "0984521773b462c460df",
+    "url": "/chessboard/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3e00f4e44bcf82d7e8b2",
+    "url": "/chessboard/static/js/2.7f67cc8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chessboard/static/js/2.7f67cc8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0984521773b462c460df",
+    "url": "/chessboard/static/js/main.152e161b.chunk.js"
+  },
+  {
+    "revision": "5448729af5d4d2d3cded",
+    "url": "/chessboard/static/js/runtime-main.67e27ef3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chessboard/static/media/logo.5d5d9eef.svg"
+  }
+]);
